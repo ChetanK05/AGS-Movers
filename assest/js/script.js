8aad541f6180f51d8d1ea2a761ad7b339c7aa083
ChetanK05 <kumbharchetan86@gmail.com>
2023-06-05 16:46:59 +0530

    msg2

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,3 +1,13 @@
 $(function(){
     $('.selectpicker').selectpicker();
 });
+// function toggleHide(){
+//     let megabtn = document.getElementById('megabtn');
+//     let megamenu = document.getElementById('megamenu');
+//     if(megamenu.style.display != 'none'){
+//         megamenu.style.display ='none';
+//     }
+//     else{
+//         megamenu.style.display = 'block';
+//     }
+// }
