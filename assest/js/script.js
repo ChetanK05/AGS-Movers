99be819dfa7e8d642f52808c21b5d8b37e701cca
ChetanK05 <kumbharchetan86@gmail.com>
2023-06-07 16:10:15 +0530

    msg3

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,13 +1,12 @@
 $(function(){
     $('.selectpicker').selectpicker();
 });
-// function toggleHide(){
-//     let megabtn = document.getElementById('megabtn');
-//     let megamenu = document.getElementById('megamenu');
-//     if(megamenu.style.display != 'none'){
-//         megamenu.style.display ='none';
-//     }
-//     else{
-//         megamenu.style.display = 'block';
-//     }
-// }
+function megamenu(){
+    let megamenu = document.getElementById('megamenu');
+    if(megamenu.style.display == 'none'){
+        megamenu.style.display ='block';
+    }
+    else{
+        megamenu.style.display = 'none';
+    }
+}
