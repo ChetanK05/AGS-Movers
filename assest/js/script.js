dade82eec6fc5a36df5e0db7fe82a66bb8ea5ae4
ChetanK05 <kumbharchetan86@gmail.com>
2023-06-07 17:29:38 +0530

    msg2

--- assest/js/script.js
+++ assest/js/script.js
@@ -10,3 +10,12 @@ function megamenu(){
         megamenu.style.display = 'none';
     }
 }
+function megamenu2(){
+    let megamenu = document.getElementById('megamenu2');
+    if(megamenu.style.display == 'none'){
+        megamenu.style.display ='block';
+    }
+    else{
+        megamenu.style.display = 'none';
+    }
+}
